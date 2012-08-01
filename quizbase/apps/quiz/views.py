ba023c4716736734d21131d2b84a36573050d2e0
Jonatan Bergkvist <jonatan.bergkvist@gmail.com>
2012-08-02 01:50:55 +0200

    Quiz collections is partially implemented. quizbase/apps/quiz/views.py got a facelift Some trimming of url.py

--- quizbase/apps/quiz/views.py
+++ quizbase/apps/quiz/views.py
@@ -3,19 +3,31 @@ from django.http import HttpResponseRedirect, HttpResponse
 from django.core.urlresolvers import reverse
 from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
-from quizbase.apps.quiz.models import Question, Choice
+from quizbase.apps.quiz.models import *
 
-def index(request):
-    questionList = Question.objects.all()
-    return render_to_response("quiz/index.html", {"questionList": questionList})
+#def index(request): ## I assume there's no reason to have this
+#    questionList = Question.objects.all()
+#    return render_to_response("quiz/index.html", {"questionList": questionList})
 
-def question(request, question_id):
+def listCollections(request):
+    collectionList = Collection.objects.all()
+    return render_to_response("quiz/collections.html",
+                              {"collectionList": collectionList})
+
+def browseCollection(request, collection_id):
+    collection = Collection.objects.get(pk=collection_id)
+    questionList = collection.question_set.all()
+    return render_to_response("quiz/questions.html",
+                              {"collection": collection,
+                               "questionList": questionList})
+
+def viewQuestion(request, question_id):
     q = Question.objects.get(pk=question_id)
     return render_to_response("quiz/question.html", 
                               {"question": q},
                               context_instance=RequestContext(request))
 
-def correct(request, question_id):
+def correctQuestion(request, question_id):
     q = Question.objects.get(pk=question_id)
     selectedChoice = q.choice_set.get(pk=request.POST['choice'])
     correctChoice = q.correctanswer_set.get(pk=question_id)
@@ -29,16 +41,16 @@ def correct(request, question_id):
                               {"question": q, "corrected": corrected},
                               context_instance=RequestContext(request))
 
-def previous(request, question_id):
+def previousQuestion(request, question_id):
     question_id = int(question_id) -1
     if question_id < 1:
         question_id = len(Question.objects.all())
     
-    return HttpResponseRedirect(reverse("quizbase.apps.quiz.views.question", args={question_id}))
+    return HttpResponseRedirect(reverse("quizbase.apps.quiz.views.viewQuestion", args={question_id}))
 
-def next(request, question_id):
+def nextQuestion(request, question_id):
     question_id = int(question_id) +1
     if question_id > len(Question.objects.all()):
         question_id = 1
     
-    return HttpResponseRedirect(reverse("quizbase.apps.quiz.views.question", args={question_id}))
+    return HttpResponseRedirect(reverse("quizbase.apps.quiz.views.viewQuestion", args={question_id}))
--- quizbase/urls.py
+++ quizbase/urls.py
@@ -8,14 +8,23 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'quizbase.views.home', name='home'),
     # url(r'^quizbase/', include('quizbase.foo.urls')),
-    url(r'^quiz/$', 'quizbase.apps.quiz.views.index'),
-    url(r'^question/(?P<question_id>\d+)/$', 'quizbase.apps.quiz.views.question'),
-    url(r'^question/(?P<question_id>\d+)/correct/$', 'quizbase.apps.quiz.views.correct'),
-    url(r'^question/(?P<question_id>\d+)/previous/$', 'quizbase.apps.quiz.views.previous'),
-    url(r'^question/(?P<question_id>\d+)/next/$', 'quizbase.apps.quiz.views.next'),
+    #url(r'^quiz/$', 'quizbase.apps.quiz.views.index'),
+    #url(r'^question/(?P<question_id>\d+)/$', 'quizbase.apps.quiz.views.question'),
+    #url(r'^question/(?P<question_id>\d+)/correct/$', 'quizbase.apps.quiz.views.correct'),
+    #url(r'^question/(?P<question_id>\d+)/previous/$', 'quizbase.apps.quiz.views.previous'),
+    #url(r'^question/(?P<question_id>\d+)/next/$', 'quizbase.apps.quiz.views.next'),
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
+)
+
+urlpatterns += patterns('quizbase.apps.quiz.views',
+                        url(r'^quiz/$', 'listCollections'),
+                        url(r'^quiz/collection/(?P<collection_id>\d+)/$', 'browseCollection'),
+                        url(r'^quiz/collection/question/(?P<question_id>\d+)/$', 'viewQuestion'),
+                        url(r'^quiz/collection/question/(?P<question_id>\d+)/correct/$', 'correctQuestion'),
+                        url(r'^quiz/collection/question/(?P<question_id>\d+)/next/$', 'nextQuestion'),
+                        url(r'^quiz/collection/question/(?P<question_id>\d+)/previous/$', 'previousQuestion'),
 )
